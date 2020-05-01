240ef5f73ea9501c1111f007fb47c7a28ed5728e
KumHoe Lau <nerojz86@gmail.com>
2020-05-01 18:04:55 +0800

    feat: Calculate the total wealth
    Use .reduce() to calculate the total wealthness.

--- Dom-Array/script.js
+++ Dom-Array/script.js
@@ -54,6 +54,18 @@ function showMillionaires() {
   updateDOM();
 }
 
+// Calculate the total wealth
+function calculateWealth() {
+  const wealth = data.reduce((acc, user) => acc + user.money, 0);
+
+  const wealthEl = document.createElement("div");
+  wealthEl.innerHTML = `<h3>Total wealth: <strong> ${formatMoney(
+    wealth
+  )}</strong></h3>`;
+
+  main.appendChild(wealthEl);
+}
+
 // Add new obj to data arr
 function addData(obj) {
   data.push(obj);
@@ -87,3 +99,4 @@ addUserBtn.addEventListener("click", getRandomUser);
 doubleBtn.addEventListener("click", doubleMoney);
 sortBtn.addEventListener("click", sortByRichest);
 showMillionairesBtn.addEventListener("click", showMillionaires);
+calculateWealthBtn.addEventListener("click", calculateWealth);
